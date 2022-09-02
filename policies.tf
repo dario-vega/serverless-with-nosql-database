f9999e56e0893c6b6330fcb1cf073e549d996c91
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-09-02 06:43:53 +0200

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -3,16 +3,16 @@
 
 # Functions Policies
 
-resource "oci_identity_policy" "NOSQLDEMOFunctionsServiceReposAccessPolicy" {
-  provider = oci.homeregion
-  name = "${var.ocir_repo_name}_FunctionsServiceReposAccessPolicy"
-  description = "${var.ocir_repo_name}_FunctionsServiceReposAccessPolicy"
-  compartment_id = var.tenancy_ocid
-  statements = ["Allow service FaaS to read repos in tenancy", "Allow service FaaS to use virtual-network-family in tenancy"]
-  provisioner "local-exec" {
-       command = "sleep 5"
-  }
-}
+# resource "oci_identity_policy" "NOSQLDEMOFunctionsServiceReposAccessPolicy" {
+#   provider = oci.homeregion
+#   name = "${var.ocir_repo_name}_FunctionsServiceReposAccessPolicy"
+#   description = "${var.ocir_repo_name}_FunctionsServiceReposAccessPolicy"
+#   compartment_id = var.tenancy_ocid
+#   statements = ["Allow service FaaS to read repos in tenancy", "Allow service FaaS to use virtual-network-family in tenancy"]
+#   provisioner "local-exec" {
+#        command = "sleep 5"
+#   }
+# }
 
 resource "oci_identity_dynamic_group" "FunctionsServiceDynamicGroup" {
   provider = oci.homeregion
