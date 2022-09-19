9330d07c7ed934e6d27defd32c0ce9eea3b22087
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-09-19 10:52:05 +0200

    Update fn-setup.tf

--- fn-setup.tf
+++ fn-setup.tf
@@ -1,6 +1,8 @@
 ## Copyright (c) 2020, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
+
+
 resource "null_resource" "Login2OCIR" {
   provisioner "local-exec" {
     command = "echo '${var.ocir_user_password}' |  docker login ${local.ocir_docker_repository} --username ${local.ocir_namespace}/${var.ocir_user_name} --password-stdin"
